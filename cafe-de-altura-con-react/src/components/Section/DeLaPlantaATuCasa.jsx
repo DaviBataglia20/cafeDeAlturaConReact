815dacfab1bfbebaf8f84a8332c169c40e7f9db2
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-02-20 15:21:07 +0100

    de la planta a tu casa terminado

--- cafe-de-altura-con-react/src/components/Section/DeLaPlantaATuCasa.jsx
+++ cafe-de-altura-con-react/src/components/Section/DeLaPlantaATuCasa.jsx
@@ -2,8 +2,8 @@ import Button from "../Button/Button"
 import FotoMejorCafe from "../FotoMejorCafe/FotoMejorCafe"
 export default function DeLaPlantaATuCasa(){
     return(
-    <div className="px-12 py-10 bg-white">
-        <div className="flex justify-start items-center gap-6">
+    <div className="px-12 py-10 bg-MejorCafe">
+        <div className="flex justify-start items-center gap-6 ">
             <div className="flex flex-col gap-4">
             <h3 class="text-green-800 font-semibold text-lg leading-6 text-shadow-texto-sombra">De la planta a tu taza</h3>
                     <h1 className="font-semibold text-4xl text-shadow-texto-sombra">El mejor café del mundo, ahora en tu casa</h1>
